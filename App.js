94bf011908699085c67a16e35f5d0e6b591a3885
woniyoon <gellifisch@gmail.com>
2018-06-27 19:48:13 -0700

    Create RootStore

--- App.js
+++ App.js
@@ -11,6 +11,8 @@ import {
   Text,
   View
 } from 'react-native';
+import { Provider } from 'mobx-react';
+import RootStore from './store/RootStore';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -22,17 +24,19 @@ const instructions = Platform.select({
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+      <Provider rootStore={new RootStore()}>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
+          <Text style={styles.instructions}>
+            To get started, edit App.js
         </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+          <Text style={styles.instructions}>
+            {instructions}
+          </Text>
+        </View>
+      </Provider>
     );
   }
 }
---
+++ store/RootStore.js
@@ -0,0 +1,8 @@
+import firebase from 'react-native-firebase';
+
+const db = firebase.firestore();
+
+export default class RootStore {
+    constructor() {
+    }
+}
