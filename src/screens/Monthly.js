f6c5bc68732645f6fa202737ce0bd6fd891e0ce7
woniyoon <gellifisch@gmail.com>
2018-07-08 23:50:53 -0700

    Remove unnecessary lines

--- src/screens/Monthly.js
+++ src/screens/Monthly.js
@@ -4,10 +4,7 @@ import { observer, inject } from 'mobx-react';
 import { Actions } from 'react-native-router-flux';
 import { Calendar } from 'react-native-calendars';
 import _ from 'lodash';
-import firebase from 'react-native-firebase';
 import AddPost from './AddPost';
-
-const db = firebase.firestore();
 
 @inject('rootStore')
 @observer
@@ -55,13 +52,6 @@ export default class Monthly extends Component {
         } else {
             this.setState({ isDialogVisible: true });
         }
-
-        // console.log(this.state.isDialogVisible);
-        // if (this.state.isDialogVisible) {
-        //     console.log('test');
-        //     this.renderDialog(date, this.user.markedDates[date], this.selectedPaletteID);
-        //     // Actions.addPost({ date, post: this.user.markedDates[date], selectedPaletteID: this.selectedPaletteID });    
-        // }
     }
 
     renderDialog(date, selectedPaletteID) {
@@ -72,32 +62,20 @@ export default class Monthly extends Component {
                 animationType={'fade'}
                 onRequestClose={() => { this.setState({ isDialogVisible: !this.state.isDialogVisible }); }}
             >
-                <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                     <View style={styles.Alert_Main_View}>
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 10 }}>
+                            <TouchableOpacity
+                                onPress={() => { this.setState({ isDialogVisible: !this.state.isDialogVisible }); }}
+                            >
+                                <Text>Cancel</Text>
+                            </TouchableOpacity>
+                            <Text>{this.date}</Text>
+                            <Text>Post</Text>
+                        </View>
                         <AddPost date={date} selectedPaletteID={selectedPaletteID} />
-                        {/* <Text style={styles.Alert_Title}>Custom Alert Dialog Title.</Text>
-                    <View style={{ width: '100%', height: 2, backgroundColor: '#fff' }} />
-                    <Text style={styles.Alert_Message}> Are You Sure(Alert Dialog Message). </Text>
-                    <View style={{ width: '100%', height: 1, backgroundColor: '#fff' }} />
-                    <View style={{ flexDirection: 'row', height: '30%' }}> */}
-                        {/* <TouchableOpacity
-                            style={styles.buttonStyle}
-                            onPress={this.ok_Button}
-                            activeOpacity={0.7}
-                        >
-                            <Text style={styles.TextStyle}> OK </Text>
-                        </TouchableOpacity>
-                        <View style={{ width: 1, height: '100%', backgroundColor: '#fff' }} />
-                        <TouchableOpacity
-                            style={styles.buttonStyle}
-                            onPress={() => { this.setState({ isDialogVisible: !this.state.isDialogVisible }); }}
-                            activeOpacity={0.7}
-                        >
-                            <Text style={styles.TextStyle}> CANCEL </Text> */}
-                        {/* </TouchableOpacity> */}
                     </View>
                 </View>
-                {/* </View> */}
             </Modal>
         );
     }
@@ -110,7 +88,6 @@ export default class Monthly extends Component {
                         width: 350,
                         height: 500
                     }}
-                    // markedDates={test}
                     markedDates={this.user.markedDates}
                     markingType={'custom'}
                     // hideArrows
@@ -146,46 +123,13 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: 'white',
     },
-    MainContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: (Platform.OS === 'ios') ? 20 : 0
-    },
     Alert_Main_View: {
-        alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'space-around',
         backgroundColor: '#f4f4f4',
-        height: 350,
+        height: 300,
         width: '90%',
         borderWidth: 1,
         borderColor: '#fff',
         borderRadius: 7,
     },
-    Alert_Title: {
-        fontSize: 25,
-        color: 'black',
-        textAlign: 'center',
-        padding: 10,
-        height: '28%'
-    },
-    Alert_Message: {
-        fontSize: 22,
-        color: 'black',
-        textAlign: 'center',
-        padding: 10,
-        height: '42%'
-    },
-    buttonStyle: {
-        width: '50%',
-        height: '100%',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    TextStyle: {
-        color: 'black',
-        textAlign: 'center',
-        fontSize: 22,
-        marginTop: -5
-    }
 });
