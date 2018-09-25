491c252e3110db81f8c9d8c9f9b5518dd41b8cc5
woniyoon <gellifisch@gmail.com>
2018-09-24 19:39:20 -0700

    Remove unnecessary line

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -12,7 +12,6 @@ import {
 } from 'react-native';
 import { observer, inject } from 'mobx-react';
 import ResetEmailDialog from '../components/ResetEmailDialog';
-import icon from '../../resource/mood_jar_icon.png';
 
 const { width } = Dimensions.get('window');
 // const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
