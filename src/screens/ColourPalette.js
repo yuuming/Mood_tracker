bd1c1951b6f1a6a2cbe5e6b7fcd52850be71bd5f
Yuumi <yuuming.n@gmail.com>
2018-09-25 00:04:20 -0700

    Create colour palette bar in cpalette page

--- src/screens/ColourPalette.js
+++ src/screens/ColourPalette.js
@@ -18,10 +18,21 @@ export default class ColourPalette extends Component {
   }
 
   render() {
+    const selectedPalette = this.rootStore.moodPaletteList[
+      this.accountStore.currentPaletteID
+    ];
     return (
       <View style={styles.container}>
-        {/* <Text>ColourPalette page!!</Text> */}
-        <MoodPalette />
+        <View style={styles.colourPalette}>
+          {paletteStyle(selectedPalette.moodColors.high, 'high')}
+          {paletteStyle(selectedPalette.moodColors.happy, 'happy')}
+          {paletteStyle(selectedPalette.moodColors.neutral, 'neutral')}
+          {paletteStyle(selectedPalette.moodColors.unhappy, 'unhappy')}
+          {paletteStyle(selectedPalette.moodColors.bad, 'bad')}
+        </View>
+        <View style={{ flex: 1 }}>
+          <MoodPalette />
+        </View>
       </View>
     );
   }
@@ -31,5 +42,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#ffffff'
+  },
+  colourPalette: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    height: 50,
+    margin: 11,
+  },
+  paletteTextStyle: {
+    fontSize: 14,
+    fontWeight: '200',
+    textAlign: 'center'
   }
 });
+
+const paletteStyle = (color, moodName) => (
+  <View style={{ flex: 1 }}>
+    <View
+      style={{
+        backgroundColor: color,
+        height: 30
+      }}
+    />
+    <Text style={styles.paletteTextStyle}>{moodName}</Text>
+  </View>
+);
